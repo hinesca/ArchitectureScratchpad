c33cf409234a2bf645c8f102b14665e4a93fc2d7
hinesca <hinesca@oregonstate.edu>
2022-04-02 11:34:36 -0700

    adding back some lost code

--- ArchitectureScratchpad/Physics/Trajectory.cs
+++ ArchitectureScratchpad/Physics/Trajectory.cs
@@ -4,29 +4,81 @@ namespace Physics
 {
      public class Trajectory
      {
-          public Trajectory(TrajectoryType trajectoryType, double[] initialPosition,
-               double[] initialVelocity, double[] finalPosition = null)
+          public Trajectory(STPosition position)
           {
-               TrajectoryType = trajectoryType;
-               InitialPosition = initialPosition;
-               InitialVelocity = initialVelocity;
-               FinalPosition = finalPosition;
+               Path = new STPosition[] { position };
+               TrajectoryType = TrajectoryType.Grounded;
           }
 
-          public Trajectory(Trajectory parent, double[] parentOffset)
+          public Trajectory(STPosition initialPosition, STPosition finalPosition)
+          {
+               Path = new STPosition[] { initialPosition, finalPosition };
+               TrajectoryType = TrajectoryType.Linear;
+          }
+
+          public Trajectory(STPosition[] path)
+          {
+               Path = path;
+               TrajectoryType = TrajectoryType.Path;
+          }
+
+          public Trajectory(Trajectory parent, STPosition parentOffset)
           {
                ParentTrajectory = parent;
                ParentOffset = parentOffset;
           }
           
           public TrajectoryType TrajectoryType { get; set; }
-          public DateTime InitialTime { get; set; } = DateTime.Now;
-          public double[] InitialVelocity { get; set; }
-          public double[] InitialPosition { get; set; }
-          public double[] FinalPosition { get; set; }          
+          public STPosition[] Path { get; set; }
           public Trajectory ParentTrajectory { get; set; }
-          public double[] ParentOffset { get; set; }
+          public STPosition ParentOffset { get; set; }
+
+          public static STPosition GetPosition(Trajectory trajectory)
+          {
+               switch (trajectory.TrajectoryType)
+               {
+                    case TrajectoryType.Grounded:
+                         return trajectory.Path[0];
+                    case TrajectoryType.Linear:
+                         return GetLinearPosition();
+                    case TrajectoryType.Path:
+                         return GetPositionOnPath();
+                    case TrajectoryType.Offset:
+                         return GetOffsetPosition();
+                    case TrajectoryType.Orbit:
+                         return GetOrbitalPosition();
+                    default:
+                         return null;
+               }
+          }
+
+          // ******************  TODO Implement with parameters that makes sense **************************************
+          // ******************  Create and update trajectories and physical objects as needed ************************
+          private static STPosition GetLinearPosition()
+          {
+               throw new NotImplementedException();
+          }
+
+          private static STPosition GetOffsetPosition()
+          {
+               throw new NotImplementedException();
+          }
+
+          private static STPosition GetOrbitalPosition()
+          {
+               throw new NotImplementedException();
+          }
+
+          private static STPosition GetPositionOnPath()
+          {
+               throw new NotImplementedException();
+          }
+          // ****************************  TODO Implement with parameters that makes sense ****************************
+
+
+
+
      }
 
-     public enum TrajectoryType { Ground, Linear, Orbit, Offset, Simulated }
+     public enum TrajectoryType { Grounded, Linear, Path, Offset, Orbit }
 }
---
+++ ArchitectureScratchpad/Physics/Vector.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Physics
+{
+     public class Vector : IEnumerable<double>
+     {
+          public Vector(double x = 0, double y = 0, double z = 0)
+          {
+               X = x;
+               Y = y;
+               Z = z;
+          }
+
+          private double[] _backingArray = new double[] {0, 0, 0};
+          public double X
+          { 
+               get { return _backingArray[0]; }
+               set { _backingArray[0] = value; }
+          }
+          public double Y
+          {
+               get { return _backingArray[1]; }
+               set { _backingArray[1] = value; }
+          }
+          public double Z
+          {
+               get { return _backingArray[2]; }
+               set { _backingArray[2] = value; }
+          }
+
+          public double this[int index]
+          {
+               get { return _backingArray[index]; }
+               set { _backingArray[index] = value; }
+          }
+
+          // TODO check the math, finish implementation
+          public Vector Normalized()
+          {
+               return this * (1 / Magnitude);
+          }
+
+          public double Magnitude
+          {
+               get { return Math.Sqrt(X * X + Y * Y + Z * Z); }
+          }
+
+          public int Count => throw new NotImplementedException();
+
+          public bool IsSynchronized => throw new NotImplementedException();
+
+          public object SyncRoot => throw new NotImplementedException();
+
+          public bool IsReadOnly => throw new NotImplementedException();
+
+          public static Vector operator +(Vector a) => a;
+          public static Vector operator -(Vector a) => new Vector(-a.X, -a.Y, -a.Z);
+          public static Vector operator +(Vector a, Vector b)
+              => new Vector(a.X + b.X, b.Y + a.Y, a.Z + b.Z);
+
+          public static Vector operator -(Vector a, Vector b)
+              => a + (-b);
+
+          public static Vector operator *(Vector a, Vector b)
+              => throw new NotImplementedException();//new Vector(a.X * b.X, a.Y * b.Y, a.Z * a.Z);
+
+          public static Vector operator *(Vector a, double s)
+              => new Vector(a.X * s, a.Y * s, a.Z);
+
+
+          public override string ToString() => $"({X}, {Y}, {Z})";
+
+          public IEnumerator GetEnumerator()
+          {
+               yield return X;
+               yield return Y;
+               yield return Z;
+          }
+
+          public void Clear()
+          {
+               X = 0;
+               Y = 0;
+               Z = 0;
+          }
+
+          IEnumerator<double> IEnumerable<double>.GetEnumerator()
+          {
+               yield return X;
+               yield return Y;
+               yield return Z;
+          }
+     }
+}
